4ff56a9125616d5c05dd71e40d30db26583c82bc
Kowshik1212 <mkowshikreddy@gmail.com>
2024-01-11 13:11:08 +0530

    runtime polymorphism is displayed here

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    String meth(){
+        return "Method is overridden in Extended class B";
+    }
+}
 public class Main {
-  
+
+    public static void main(String[] args){
+        A obj1=new A();
+        A obj2=new B();
+        B obj3=new B();
+
+        System.out.println(obj1.meth());
+        System.out.println(obj2.meth());
+        System.out.println(obj3.meth());
+    }
+
 }
